57204a7a1ad28397665c3c2e08b350593a7dc303
Hasnain Ahmed Alve <51086057+has9alv3@users.noreply.github.com>
2023-04-30 01:46:56 +0600

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,8 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+// comments variables
+const commentsItem = document.querySelectorAll("[data-comments-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -29,20 +29,20 @@ const modalText = document.querySelector("[data-modal-text]");
 const modalTime = document.querySelector("[data-modal-time]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const commentsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+for (let i = 0; i < commentsItem.length; i++) {
 
-  testimonialsItem[i].addEventListener("click", function () {
+  commentsItem[i].addEventListener("click", function () {
 
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalImg.src = this.querySelector("[data-comments-avatar]").src;
+    modalImg.alt = this.querySelector("[data-comments-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-comments-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-comments-text]").innerHTML;
     /*
     switch (modalTitle.innerHTML){
       case "SW마이스터고 연합해커톤":
@@ -56,15 +56,15 @@ for (let i = 0; i < testimonialsItem.length; i++) {
         break;
     }
     */
-    testimonialsModalFunc();
+    commentsModalFunc();
 
   });
 
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+modalCloseBtn.addEventListener("click", commentsModalFunc);
+overlay.addEventListener("click", commentsModalFunc);
 
 
 
